a5bd0b31ce29505155fc3e1c349a61081f67761a
JiangT <jtao@JiangTdeMacBook-Pro.local>
2019-03-17 22:25:55 +0800

    初始化搜索界面

---
+++ app/src/main/java/com/jtao/gankdemo/Activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.jtao.gankdemo.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.jtao.gankdemo.R;
+
+public class SearchActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        
+    }
+}
