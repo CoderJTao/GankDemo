1a78f3ce65d359008d3105641237b9f30b941d55
JiangT <jtao@JiangTdeMacBook-Pro.local>
2019-03-21 22:13:35 +0800

    首页添加头部视图

--- app/src/main/java/com/jtao/gankdemo/Activity/Adapter/NewsAdapter.java
+++ app/src/main/java/com/jtao/gankdemo/Activity/Adapter/NewsAdapter.java
@@ -21,7 +21,7 @@ import com.jtao.gankdemo.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsRecycleHolder> {
+public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context mContext;
 
@@ -30,6 +30,9 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsRecycleHol
     private List<NewsSubMoshi> newsList;
 
     private List<String> tagLists;
+
+    private static final int TYPE_HEADER = 1;
+    private static final int TYPE_ITEMS  = 2;
 
     // 定义一个接口
     public interface OnNewsItemClickListener {
@@ -67,48 +70,89 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsRecycleHol
         });
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADER;
+        } else {
+            return TYPE_ITEMS;
+        }
+    }
+
     @NonNull
     @Override
-    public NewsRecycleHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycle_news_item, viewGroup, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view;
 
-        return new NewsRecycleHolder(view);
+        if (i == TYPE_HEADER) {
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.news_item_header, viewGroup, false);
+            return new NewsHeaderHolder(view);
+        } else  {
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycle_news_item, viewGroup, false);
+            return new NewsRecycleHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NewsRecycleHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
         if (mNews == null || newsList.size() == 0 || newsList.size() <= i) return;
 
-        // 设置值
-        final NewsSubMoshi item = newsList.get(i);
+        if (getItemViewType(i) == TYPE_HEADER) {
+            // header
+            String imgUrl = mNews.todayGirl;
 
-        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onClickItem(item);
+            NewsHeaderHolder holder = (NewsHeaderHolder)viewHolder;
+
+            if (!imgUrl.equals("")) {
+                Glide.with(mContext).load(imgUrl).into(holder.new_header);
             }
-        });
+        } else {
+            // items
+            final NewsSubMoshi item = newsList.get(i - 1);
 
-        if (item != null) {
-            viewHolder.new_desc.setText(item.desc);
-            viewHolder.new_who.setText(item.who);
-            viewHolder.new_time.setText(item.publishedAt.substring(0, 10));
+            NewsRecycleHolder holder = (NewsRecycleHolder)viewHolder;
 
-            if (item.images != null) {
-                viewHolder.new_showImage.setVisibility(View.VISIBLE);
-                String url = item.images.get(0);
-                Glide.with(mContext).load(url).into(viewHolder.new_showImage);
-            } else {
-                viewHolder.new_showImage.setVisibility(View.GONE);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onClickItem(item);
+                }
+            });
+
+            if (item != null) {
+                holder.new_desc.setText(item.desc);
+                holder.new_who.setText(item.who);
+                holder.new_time.setText(item.publishedAt.substring(0, 10));
+
+                if (item.images != null) {
+                    holder.new_showImage.setVisibility(View.VISIBLE);
+                    String url = item.images.get(0);
+                    Glide.with(mContext).load(url).into(holder.new_showImage);
+                } else {
+                    holder.new_showImage.setVisibility(View.GONE);
+                }
             }
         }
     }
 
     @Override
     public int getItemCount() {
-        return newsList.size();
+        return newsList.size() + 1;
     }
 
+    public static class NewsHeaderHolder extends RecyclerView.ViewHolder {
+        public final ImageView new_header;
+
+        public NewsHeaderHolder(@NonNull View itemView) {
+            super(itemView);
+
+            new_header = itemView.findViewById(R.id.news_header_img);
+        }
+    }
+
+    /**
+     * 每一个item的holder
+     */
     public static class NewsRecycleHolder extends RecyclerView.ViewHolder {
         public final TextView new_desc;
         public final TextView new_who;
@@ -124,4 +168,6 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsRecycleHol
             new_showImage = itemView.findViewById(R.id.new_item_showimg);
         }
     }
+
+
 }
